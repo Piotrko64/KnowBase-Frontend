4d4594be9c2d6425384a36f34b19f56f5aa6c6da
Piotrko64 <Piotrpc77@gmail.com>
2024-06-17 21:19:21 +0200

    ADD: experiments basic buttons tailwind classes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPanelComponent } from './core/pages/auth/components/login-panel/login-panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginPanelComponent,
+  },
+];
