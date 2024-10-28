76db0484314aae547c9f8a2f0918052f91ffe404
Piotrko64 <Piotrpc77@gmail.com>
2024-10-28 18:15:57 +0100

    INSTALL: package angular icons

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { octListUnordered } from '@ng-icons/octicons';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
+  viewProviders: [provideIcons({ octListUnordered })],
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
