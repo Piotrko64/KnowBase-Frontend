17afc9e368af48db16fc51f8c32523d41743f074
Piotrko64 <PiotrPc77@gmail.com>
2025-01-16 22:03:36 +0100

    INSTALL: Fontawesome icons

--- src/app/features/create-book/creating-panel/creating-panel.component.ts
+++ src/app/features/create-book/creating-panel/creating-panel.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { octListUnordered } from '@ng-icons/octicons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-creating-panel',
   standalone: true,
-  imports: [NgIcon],
+  imports: [FontAwesomeModule],
   templateUrl: './creating-panel.component.html',
   styleUrl: './creating-panel.component.scss',
-  viewProviders: [provideIcons({ octListUnordered })],
 })
 export class CreatingPanelComponent {
+
+  faCoffee = faCoffee;
 
 }
